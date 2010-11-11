983fa2dc7638c8bc02eb4efedb0cc611d40ea693
Josh Butts <josh@joshbutts.com>
2010-11-10 18:33:27 -0600

    update homework assignment

--- class-1/homework.php
+++ class-1/homework.php
@@ -1,2 +1,27 @@
 <?php
-//problem 1
+
+$colors = array('red', 'green', 'blue', 'yellow', 'purple');
+
+$numbers = array("lessthan10" => array(1,2,3,4,5,6,7,8,9),
+                 "lessthan20" => array(10,11,12,13,14,15,16,17,18,19));
+
+// #1
+// update your list of colors to be sorted in alphabetical order
+
+// #2
+// print out your list of colors, each one on a new line
+
+// #3
+// create another array of colors that are available as M&M
+// create a new array that contains only the items from $colors that 
+// are available as M&M's
+
+
+// #4
+// calculate the sum of all the numbers contained in $numbers
+
+// #5
+// Write a program that prints the numbers from 1 to 100. 
+// But for multiples of three print “Fizz” instead of the number and for 
+// the multiples of five print “Buzz”. For numbers which are multiples 
+// of both three and five print “FizzBuzz”.
