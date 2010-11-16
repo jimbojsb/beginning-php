fe5dd70688d08dc5ddb6d313a0e55cc76b01547a
Josh Butts <josh@joshbutts.com>
2010-11-16 08:16:17 -0600

    add in class exercise for class 2

---
+++ class-2/inclasscoding.php
@@ -0,0 +1,14 @@
+<?php
+// write a function that fills creates an array with 10 
+// random numbers as elements (http://php.net/rand)
+
+// write your own function to calculate the average value
+// for an array of numbers
+
+// finally, write a function that generates 10 random arrays 
+// using the first function. then calculate the average values
+// of those arrays. Output the averages, each on it's own line
+
+
+
+// Now that you have that last function written, call it.
