6cf0472f81bd52ff5a48d16ae4102e22f9505c92
Josh Butts <josh@joshbutts.com>
2011-01-05 17:12:00 -0600

    add class 6 files

---
+++ class-6/class.php
@@ -0,0 +1,11 @@
+<?php
+class Person 
+{
+    public $name;
+    public $age;
+    
+    public function __construct()
+    {
+        
+    }
+}
---
+++ class-6/demo.php
@@ -0,0 +1,14 @@
+<pre>
+<?php
+require_once 'class.php';
+
+$person = new Person();
+
+
+
+$person->age = 17;
+
+var_dump($person);
+
+?>
+</pre>
